89e7f746d456825544bd95ffa7b5255b7c263cf4
Nymnion <nymnhs@gmail.com>
2024-10-24 16:16:16 +0200

    V0.11

--- js/messageHandler.js
+++ js/messageHandler.js
@@ -1,6 +1,27 @@
 // Chat container reference
 const chatContainer = document.getElementById('chat-container');
 
+// Default Twitch colors for users who haven't set a color
+const DEFAULT_COLORS = [
+    '#FF0000', '#0000FF', '#008000', '#B22222', '#FF7F50',
+    '#9ACD32', '#FF4500', '#2E8B57', '#DAA520', '#D2691E',
+    '#5F9EA0', '#1E90FF', '#FF69B4', '#8A2BE2', '#00FF7F'
+];
+
+// Get color for user - either their set color or a consistent generated one
+function getUserColor(tags) {
+    if (tags.color) {
+        return tags.color;
+    }
+    
+    // Generate consistent color based on username
+    const hash = tags['display-name'].split('').reduce((acc, char) => {
+        return char.charCodeAt(0) + ((acc << 5) - acc);
+    }, 0);
+    
+    return DEFAULT_COLORS[Math.abs(hash) % DEFAULT_COLORS.length];
+}
+
 // Sanitize text content
 function sanitizeText(text) {
     const div = document.createElement('div');
@@ -16,6 +37,7 @@ function onMessageHandler(target, context, msg, self) {
     const usernameSpan = document.createElement('span');
     usernameSpan.className = 'username';
     usernameSpan.textContent = context['display-name'];
+    usernameSpan.style.color = getUserColor(context);
 
     const messageSpan = document.createElement('span');
     messageSpan.className = 'message';
